2a3de2265544c8a08997b464fd7e3148de042525
Colin Sandlin <csandlin010@gmail.com>
2019-07-03 10:55:25 -0500

    ready to deploy

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -24,7 +24,7 @@ module.exports = {
         }
     },
     app: {
-        src: ["../scripts/main.js"],
-        dest: "../../public/bundle.js"
+        src: ["./src/scripts/main.js"],
+        dest: "./public/bundle.js"
     }
 }
--- grunt/watch.js
+++ grunt/watch.js
@@ -1,7 +1,7 @@
 module.exports = {
     scripts: {
         files: [
-            "../scripts/**/*.js",
+            "./src/scripts/**/*.js",
             "!node_modules/**/*.js"
         ],
         tasks: ["eslint", "browserify"],
